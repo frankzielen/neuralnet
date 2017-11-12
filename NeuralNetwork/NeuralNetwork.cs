66bd8a67590f53342cffa9b9052a6c04bd8222e9
frankzielen <frank.zielen@z-m-b.de>
2017-11-12 23:55:24 +0100

    Total MNIST Datasets included (60k train, 10k test)

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -13,54 +13,75 @@ namespace NeuralNetwork
         public App()
         {
             // Setup neural net
-            NeuralNet neuralnet = new NeuralNet(28*28,100,10,0.3);
+            NeuralNet neuralnet = new NeuralNet(28 * 28, 100, 10, 0.3);
 
             // Setup MNIST data manager
             MNISTDataManager mnistdata = new MNISTDataManager();
 
-            // Read train data
-            mnistdata.ReadEmbeddedText(@"NeuralNetwork.MNISTDatasets.mnist_train_100.csv");
-
-            // Train neural net
-            for (int i = 0; i < mnistdata.CountData; i++)
-                neuralnet.Train(mnistdata.Input(i), mnistdata.Output(i));
-            
-            // Read test data
-            mnistdata.ReadEmbeddedText(@"NeuralNetwork.MNISTDatasets.mnist_test_10.csv");
-
-            // Test neural net
-            Vector<double> scorecard = Vector<double>.Build.Dense(mnistdata.CountData);
-            string message = "Nr.\tData\tAnswer\tResult\n\n";
-
-            for (int i = 0; i < mnistdata.CountData; i++)
+            // Label to show results
+            Label label = new Label
             {
-                Vector<double> answer = neuralnet.Query(mnistdata.Input(i));
-                if (answer.AbsoluteMaximumIndex() == mnistdata.Number(i))
-                    scorecard[i] = 1.0;
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = ""
+            };
 
-                message += string.Format("{0}\t{1}\t{2}\t{3}\n", i, mnistdata.Number(i), answer.AbsoluteMaximumIndex(), scorecard[i]);
-            }
+            // Button to train net
+            Button buttontrain = new Button();
+            buttontrain.Text = "Train net";
+            buttontrain.VerticalOptions = LayoutOptions.End;
+            buttontrain.Clicked += (s, e) =>
+            {
+                // Read train data
+                mnistdata.ReadEmbeddedText(@"NeuralNetwork.MNISTDatasets.mnist_train.csv");
 
-            message += string.Format("\nPerformance = {0}", scorecard.Sum() / scorecard.Count);
+                // Train neural net
+                for (int i = 0; i < mnistdata.CountData; i++)
+                {
+                    neuralnet.Train(mnistdata.Input(i), mnistdata.Output(i));
+
+                    //if ((i + 1) % 5000 == 0)
+                    //{
+                    //    bool answer = await MainPage.DisplayAlert("Read Data", String.Format("Net trained with {0} numbers. Continue?", i+1), "Yes", "No");
+                    //    if (!answer)
+                    //        break;
+                    //}
+                }
+            };
+
+            // Button to test net
+            Button buttontest = new Button();
+            buttontest.Text = "Test net";
+            buttontest.VerticalOptions = LayoutOptions.End;
+            buttontest.Clicked += (s, e) =>
+            {
+                // Read test data
+                mnistdata.ReadEmbeddedText(@"NeuralNetwork.MNISTDatasets.mnist_test.csv");
+
+                // Test neural net
+                Vector<double> scorecard = Vector<double>.Build.Dense(mnistdata.CountData);
+                //String message = "Nr.\tData\tAnswer\tResult\n\n";
+
+                for (int i = 0; i < mnistdata.CountData; i++)
+                {
+                    Vector<double> answer = neuralnet.Query(mnistdata.Input(i));
+                    if (answer.AbsoluteMaximumIndex() == mnistdata.Number(i))
+                        scorecard[i] = 1.0;
+
+                    //message += string.Format("{0}\t{1}\t{2}\t{3}\n", i, mnistdata.Number(i), answer.AbsoluteMaximumIndex(), scorecard[i]);
+                }
+
+                //message += string.Format("\nPerformance = {0}", scorecard.Sum() / scorecard.Count);
+
+                label.Text = string.Format("Performance = {0}", scorecard.Sum() / scorecard.Count);
+            };
 
             // The root page of your application
             var content = new ContentPage
             {
                 Title = "NeuralNetwork",
-                Content = new ScrollView
+                Content = new StackLayout
                 {
-                    Content = new StackLayout
-                    {
-                        VerticalOptions = LayoutOptions.Center,
-                        Children =
-                        {
-                            new Label
-                            {
-                                HorizontalTextAlignment = TextAlignment.Center,
-                                Text = message
-                            }
-                        }
-                    }
+                    Children = { label, buttontrain, buttontest } 
                 }
             };
 
