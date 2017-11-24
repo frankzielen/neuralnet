aa974d1ba93a910a128415b6968f3789cdf56862
frankzielen <frank.zielen@z-m-b.de>
2017-11-24 23:40:40 +0100

    Take Photos included

--- NeuralNetwork/CameraTestPage.cs
+++ NeuralNetwork/CameraTestPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
+using Plugin.Media;
 
 namespace NeuralNetwork
 {
@@ -11,6 +12,49 @@ namespace NeuralNetwork
             Title = "Test Neural Net";
 
             BackgroundColor = Color.SteelBlue;
+
+            Button button = new Button { Text = "Take a Picture" };
+            Image image = new Image();
+
+            Content = new StackLayout
+            {
+                Children = {button, image}
+            };
+
+            button.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg",
+                    SaveToAlbum = true,
+                    AllowCropping = true
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("File Location", file.Path, "OK");
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+
+                //or:
+                //image.Source = ImageSource.FromFile(file.Path);
+                //image.Dispose();
+            };
         }
     }
 }
